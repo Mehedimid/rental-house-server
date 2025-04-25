7d3b6054bbf45d62b5faf443a2291117169bd450
Mehedimid <mehedihasan25276@gmail.com>
2025-04-25 19:30:57 +0600

    fixed payment

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -3,6 +3,6 @@ const frontendBaseUrl = 'https://basa-finder-client-rosy.vercel.app';
 // const frontendBaseUrl =' https://rental-house-client.vercel.app/ '
 
 // const backendBaseUrl = 'http://localhost:5000';
-const backendBaseUrl = 'https://bike-museum-server-tan.vercel.app';
+const backendBaseUrl = 'https://basa-finder-server-ten.vercel.app';
 
 export { frontendBaseUrl, backendBaseUrl };
